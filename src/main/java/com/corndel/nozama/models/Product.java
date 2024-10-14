39a20b255d04ad584ab8a3e8932532685dcdc13c
Damian <damiangildea12@gmail.com>
2024-10-14 14:18:16 +0100

    Added product model

---
+++ src/main/java/com/corndel/nozama/models/Product.java
@@ -0,0 +1,75 @@
+package com.corndel.nozama.models;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private String description;
+    private float price;
+    private int stockQuantity;
+    private String imageURL;
+
+    public Product(Integer id, String name, String description, float price, int stockQuantity, String imageURL) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.imageURL = imageURL;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stockQuantity=" + stockQuantity +
+                ", imageURL='" + imageURL + '\'' +
+                '}';
+    }
+}
