b9fbfc7d12a33042573aa771c8e58291aa31192c
Damian <damiangildea12@gmail.com>
2024-10-14 11:35:47 +0100

    Exercises 2 and 3 done

--- src/main/java/com/corndel/nozama/DB.java
+++ src/main/java/com/corndel/nozama/DB.java
@@ -1,17 +1,16 @@
 package com.corndel.nozama;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class DB {
-  private static final Dotenv dotenv = Dotenv.load();
-  public static final String dbUrl = null;
+  public static final Dotenv dotenv = Dotenv.load();
+  public static final String dbUrl = dotenv.get("DB_URL");
+
 
   public static Connection getConnection() throws SQLException {
-    var dbUrl = dotenv.get("DB_URL");
     return DriverManager.getConnection(dbUrl);
   }
 }
