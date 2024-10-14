d86994b1ce876ea4b878e422ca7c219e4b957b83
Meron G <merong@Merons-MacBook-Air.local>
2024-10-14 12:06:01 +0100

    day 1 exercises

--- src/main/java/com/corndel/nozama/DB.java
+++ src/main/java/com/corndel/nozama/DB.java
@@ -1,14 +1,18 @@
 package com.corndel.nozama;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
   /** TODO: Set the dbUrl */
-  public static final String dbUrl = null;
+  public static final Dotenv dotenv = Dotenv.load();
+  public static final String dbUrl = dotenv.get("DB_URL");
 
   public static Connection getConnection() throws SQLException {
     /** TODO: finish this method */
-    return null;
+    return DriverManager.getConnection(dbUrl);
   }
 }
