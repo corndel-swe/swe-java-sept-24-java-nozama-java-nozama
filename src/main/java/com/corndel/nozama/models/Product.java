84d6969afbd193a615cb58e083156dfa1ed695cd
Charlie-robin <charlie.robin.richardson@gmail.com>
2024-10-14 17:24:00 +0100

    Refactored Product and added create to ProductRepository

--- src/main/java/com/corndel/nozama/models/Product.java
+++ src/main/java/com/corndel/nozama/models/Product.java
@@ -1,6 +1,21 @@
 package com.corndel.nozama.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Product {
+
+    public static Product ofResultSet(ResultSet rs) throws SQLException {
+        Product product = new Product();
+        product.setId(rs.getString("id"));
+        product.setName(rs.getString("name"));
+        product.setDescription(rs.getString("description"));
+        product.setImageURL(rs.getString("imageURL"));
+        product.setPrice(rs.getFloat("price"));
+        product.setStockQuantity(rs.getInt("stockQuantity"));
+        return product;
+    }
+
 
     private String id;
     private String name;
@@ -10,15 +25,6 @@ public class Product {
     private int stockQuantity;
 
     public Product() {
-    }
-
-    public Product(String id, String name, String description, float price, int stockQuantity, String imageURL) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
-        this.imageURL = imageURL;
     }
 
     public String getId() {
--- src/main/java/com/corndel/nozama/repositories/ProductRepository.java
+++ src/main/java/com/corndel/nozama/repositories/ProductRepository.java
@@ -3,7 +3,6 @@ package com.corndel.nozama.repositories;
 import com.corndel.nozama.DB;
 import com.corndel.nozama.models.Product;
 
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,14 +18,7 @@ public class ProductRepository {
 
             var products = new ArrayList<Product>();
             while (rs.next()) {
-                Product product = new Product();
-                product.setId(rs.getString("id"));
-                product.setName(rs.getString("name"));
-                product.setDescription(rs.getString("description"));
-                product.setImageURL(rs.getString("imageURL"));
-                product.setPrice(rs.getFloat("price"));
-                product.setStockQuantity(rs.getInt("stockQuantity"));
-                products.add(product);
+                products.add(Product.ofResultSet(rs));
             }
 
             return products;
@@ -35,7 +27,6 @@ public class ProductRepository {
 
     public static Product findById(String id) throws SQLException {
         var query = "SELECT * FROM products WHERE id = ?";
-
 
         try (var con = DB.getConnection();
              var stmt = con.prepareStatement(query);
@@ -48,15 +39,30 @@ public class ProductRepository {
                     return null;
                 }
 
-                Product product = new Product();
-                product.setId(rs.getString("id"));
-                product.setName(rs.getString("name"));
-                product.setDescription(rs.getString("description"));
-                product.setImageURL(rs.getString("imageURL"));
-                product.setPrice(rs.getFloat("price"));
-                product.setStockQuantity(rs.getInt("stockQuantity"));
+                return Product.ofResultSet(rs);
+            }
+        }
+    }
 
-                return product;
+
+    public static Product create(Product product) throws SQLException {
+        String query = "INSERT INTO products (name,description,price,stockQuantity,imageURL) VALUES(?,?,?,?,?) RETURNING *";
+
+        try (var connection = DB.getConnection();
+             var statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, product.getName());
+            statement.setString(2, product.getDescription());
+            statement.setFloat(3, product.getPrice());
+            statement.setInt(4, product.getStockQuantity());
+            statement.setString(5, product.getImageURL());
+
+            try (var rs = statement.executeQuery();) {
+                while (!rs.next()) {
+                    return null;
+                }
+
+                return Product.ofResultSet(rs);
             }
         }
     }
@@ -64,6 +70,9 @@ public class ProductRepository {
 
     public static void main(String[] args) throws SQLException {
         System.out.println(findAll());
-        System.out.println(findById("72"));
+        Product product = findById("72");
+
+        assert product != null;
+        System.out.println(create(product));
     }
 }
