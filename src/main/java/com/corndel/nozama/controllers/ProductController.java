dd2da4829d4c2c1376b561afdf4ead7198ca5e4f
Damian <damiangildea12@gmail.com>
2024-10-15 14:08:31 +0100

    Product controllers added for get all and by ID

--- src/main/java/com/corndel/nozama/controllers/ProductController.java
+++ src/main/java/com/corndel/nozama/controllers/ProductController.java
@@ -1,4 +1,29 @@
 package com.corndel.nozama.controllers;
 
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import com.corndel.nozama.models.Product;
+import com.corndel.nozama.repositories.ProductRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
 public class ProductController {
+
+    public static void getAllProducts(Context ctx) throws SQLException {
+        List<Product> products = ProductRepository.findAll();
+        ctx.json(products);
+    }
+
+    public static void getProductById(Context ctx) throws SQLException {
+        String id = ctx.pathParam("id");
+        Product product = ProductRepository.findById(id);
+        if (product != null) {
+            ctx.json(product);
+        } else {
+            ctx.status(404).result("Product not found");
+        }
+    }
+
 }
