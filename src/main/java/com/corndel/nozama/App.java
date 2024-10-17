a6739e2e68c9af0410569a40661ddc060e87f66f
kaiclastic <halajash567@outlook.com>
2024-10-17 15:20:09 +0100

    used controller and api builder in login-user

--- src/main/java/com/corndel/nozama/App.java
+++ src/main/java/com/corndel/nozama/App.java
@@ -1,9 +1,12 @@
 package com.corndel.nozama;
 
+import com.corndel.nozama.controllers.UserController;
 import com.corndel.nozama.repositories.UserRepository;
 import io.javalin.Javalin;
 import io.javalin.http.HttpStatus;
 import com.corndel.nozama.models.User;
+import static io.javalin.apibuilder.ApiBuilder.*;
+
 
 public class App {
   private Javalin app;
@@ -14,32 +17,47 @@ public class App {
   }
 
   public App() {
-    app = Javalin.create();
-    app.get(
-        "/",
-        ctx -> {
-          var users = UserRepository.findAll();
-          ctx.json(users);
-        });
-    app.get(
-        "/users/{userId}",
-        ctx -> {
-          var id = Integer.parseInt(ctx.pathParam("userId"));
-          var user = UserRepository.findById(id);
-          ctx.status(HttpStatus.IM_A_TEAPOT).json(user);
-        });
-    app.post(
-            "/users/login",
-            ctx ->{
-                User body = ctx.bodyAsClass(User.class);
-                var user = UserRepository.loginUser(body.getUsername(),body.getPassword());
-                ctx.status(201);
-                ctx.json(user);
-            }
-    );
+      app = Javalin.create(config -> {
+          config.router.apiBuilder(() -> {
+              path("users", () -> {
+                  get("", UserController::getAllUsers);
+                  get("/{userId}", UserController::getUserById);
+                  post("/login", UserController::loginUser);
+              });
+          });
+      });
   }
 
-  public Javalin javalinApp() {
+
+
+//    app = Javalin.create();
+//    app.get(
+//        "/",
+//        ctx -> {
+//          var users = UserRepository.findAll();
+//          ctx.json(users);
+//        });
+//    app.get(
+//        "/users/{userId}",
+//        ctx -> {
+//          var id = Integer.parseInt(ctx.pathParam("userId"));
+//          var user = UserRepository.findById(id);
+//          ctx.status(HttpStatus.IM_A_TEAPOT).json(user);
+//        });
+//    app.post(
+//            "/users/login",
+//            ctx ->{
+//                User body = ctx.bodyAsClass(User.class);
+//                var user = UserRepository.loginUser(body.getUsername(),body.getPassword());
+//                ctx.status(201);
+//                ctx.json(user);
+//            }
+//    );
+
+
+
+
+    public Javalin javalinApp() {
     return app;
   }
 }
