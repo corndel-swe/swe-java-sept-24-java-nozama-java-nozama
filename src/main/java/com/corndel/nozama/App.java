42e5a287e295e6ded43545becd609705b8405ce4
pablobgldo <pablobgldo@gmail.com>
2024-10-17 15:09:18 +0100

    Refactored app

--- src/main/java/com/corndel/nozama/App.java
+++ src/main/java/com/corndel/nozama/App.java
@@ -1,66 +1,29 @@
 package com.corndel.nozama;
 
-import com.corndel.nozama.models.Review;
-import com.corndel.nozama.repositories.ReviewRepository;
-import com.corndel.nozama.repositories.UserRepository;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.corndel.nozama.controllers.ReviewController;
 import io.javalin.Javalin;
-import io.javalin.http.HttpStatus;
-
-import java.util.HashMap;
-import java.util.Map;
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class App {
   private Javalin app;
-  record ReviewRequest(Integer productId, Integer userId, Integer rating, String reviewText) {}
 
   public static void main(String[] args) {
-    var app = new App().javalinApp();
+    var app = javalinApp();
     app.start(8080);
   }
 
-  public App() {
-    app = Javalin.create();
-    app.get("/",
-        ctx -> {
-          var users = UserRepository.findAll();
-          ctx.json(users);
-        });
-    app.get(
-        "/users/{userId}",
-        ctx -> {
-          var id = Integer.parseInt(ctx.pathParam("userId"));
-          var user = UserRepository.findById(id);
-          ctx.status(HttpStatus.IM_A_TEAPOT).json(user);
-        });
+  public static Javalin javalinApp() {
 
-    // Get all reviews by product
-    app.get("/products/{productId}/reviews", ctx -> {
-        int productId = Integer.parseInt(ctx.pathParam("productId"));
-        var reviews = ReviewRepository.getReviewsByProduct(productId);
-        ctx.status(200).json(reviews);
-    });
+      return Javalin.create(config -> {
 
-    // Get average review of product
-    app.get("/products/{productId}/reviews/average", ctx -> {
-        int productId = Integer.parseInt(ctx.pathParam("productId"));
-        Double average = ReviewRepository.getAvgRatingByProduct(productId);
-        Map<String, Double> response = new HashMap<>();
-        response.put("averageRating", average);
-        ctx.status(200).json(response);
+          config.router.apiBuilder(() -> {
+              path("/products", () -> {
+                  get("/{productId}/reviews", ReviewController::getReviewByProduct);
+                  get("/{productId}/reviews/average", ReviewController::getAvgRatingByProduct);
+                  post("/{productId}/reviews", ReviewController:: createReview);
+              });
+          });
+
       });
-
-    // Create a new product review
-    app.post("/products/{productId}/reviews", ctx -> {
-        ReviewRequest body = ctx.bodyAsClass(ReviewRequest.class);
-        Review review = ReviewRepository.createReview(body.productId(), body.userId(), body.rating(), body.reviewText());
-        ctx.status(201).json(review);
-    });
-
-
-}
-
-  public Javalin javalinApp() {
-    return app;
-  }
+    }
 }
---
+++ src/main/java/com/corndel/nozama/controllers/ReviewController.java
@@ -0,0 +1,37 @@
+package com.corndel.nozama.controllers;
+
+import com.corndel.nozama.App;
+import com.corndel.nozama.models.Review;
+import com.corndel.nozama.repositories.ReviewRepository;
+import io.javalin.http.Context;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ReviewController {
+    record ReviewRequest(Integer productId, Integer userId, Integer rating, String reviewText) {}
+    public static void getReviewByProduct(Context ctx) throws SQLException {
+        int productId = Integer.parseInt(ctx.pathParam("productId"));
+        var reviews = ReviewRepository.getReviewsByProduct(productId);
+        ctx.status(200).json(reviews);
+
+    }
+
+    public static void getAvgRatingByProduct(Context ctx) throws SQLException {
+        int productId = Integer.parseInt(ctx.pathParam("productId"));
+        Double average = ReviewRepository.getAvgRatingByProduct(productId);
+        Map<String, Double> response = new HashMap<>();
+        response.put("averageRating", average);
+        ctx.status(200).json(response);
+    }
+
+    public static void createReview(Context ctx) throws SQLException {
+        ReviewRequest body = ctx.bodyAsClass(ReviewRequest.class);
+        Review review = ReviewRepository.createReview(body.productId(), body.userId(), body.rating(), body.reviewText());
+        ctx.status(201).json(review);
+
+    }
+
+}
